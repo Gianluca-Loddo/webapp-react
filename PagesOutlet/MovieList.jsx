cc6d6a07db08b78285adeb3b55e256acb5133dad
Gianluca-Loddo <gianlu.loddo@gmail.com>
2025-11-16 18:12:02 +0100

    call test backend for movielist: ok

--- PagesOutlet/MovieList.jsx
+++ PagesOutlet/MovieList.jsx
@@ -1,16 +1,29 @@
-import axios from "axios";
-import { useEffect } from "react";
-
-import Card from "../Components/Card";
+import { useEffect, useState } from "react";
+import axiosClient from "../src/axiosClient";
 
 export default function MovieList() {
 
+    const [movies, setMovies] = useState([]);
 
-    return(
+    useEffect(() => {
+        axiosClient.get("/movies")
+            .then(res => {
+                console.log("FILM FROM BACKEND:", res.data);
+                setMovies(res.data);
+            })
+            .catch(err => console.error(err));
+    }, []);
 
-        <>
-            <Card/>
-        </>
-    )
+    return (
+        <div>
+            <h1>MOVIE LIST</h1>
 
+            {movies.map(movie => (
+                <div key={movie.id}>
+                    <h3>{movie.title}</h3>
+                    <p>{movie.director}</p>
+                </div>
+            ))}
+        </div>
+    );
 }
